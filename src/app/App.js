802e8360c9404473b0a690451d1dac30582d538c
Lola Simpson <lolalsimp@gmail.com>
2018-07-07 11:22:14 -0500

    added privateroute for if login or not authed

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
+import React from 'react';
+import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
 // import Fish from '../components/Fish/Fish';
 import Home from '../components/Home/Home';
-// import Inventory from '../components/Inventory/Inventory';
+import Inventory from '../components/Inventory/Inventory';
 // import Login from '../components/Login/Login';
 import Navbar from '../components/Navbar/Navbar';
 // import New from '../components/New/New';
@@ -16,7 +16,27 @@ import Navbar from '../components/Navbar/Navbar';
 // import connection from '../firebaseRequests/connection';
 // import mashupRequests from '../firebaseRequests/animals';
 
-class App extends Component {
+const PrivateRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
+class App extends React.Component {
+  state= {
+    authed: false,
+  }
   render () {
     return (
       <div className="App">
@@ -26,7 +46,12 @@ class App extends Component {
             <div className="container">
               <div className="row">
                 <Switch>
-                  <Route path="/" exact component={Home}/>
+                  <Route path="/" exact component={Home} />
+                  <PrivateRoute
+                    path="/inventory"
+                    authed={this.state.authed}
+                    component={Inventory}
+                  />
                 </Switch>
               </div>
             </div>
