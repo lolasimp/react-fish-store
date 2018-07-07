3ec80aac4913bbc3c7371dfed1e6bbbcec0493cb
Lola Simpson <lolalsimp@gmail.com>
2018-07-07 10:52:31 -0500

    added Home

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
 import './App.css';
 
-import Fish from '../components/Fish/Fish';
+// import Fish from '../components/Fish/Fish';
 import Home from '../components/Home/Home';
-import Inventory from '../components/Inventory/Inventory';
-import Login from '../components/Login/Login';
+// import Inventory from '../components/Inventory/Inventory';
+// import Login from '../components/Login/Login';
 import Navbar from '../components/Navbar/Navbar';
-import New from '../components/New/New';
-import Order from '../components/Order/Order';
-import OrderSpa from '../components/OrderSpa/OrderSpa';
-import Register from '../components/Register/Register';
-import SingleOrder from '../components/SingleOrder/SingleOrder';
+// import New from '../components/New/New';
+// import Order from '../components/Order/Order';
+// import OrderSpa from '../components/OrderSpa/OrderSpa';
+// import Register from '../components/Register/Register';
+// import SingleOrder from '../components/SingleOrder/SingleOrder';
 
 // import connection from '../firebaseRequests/connection';
 // import mashupRequests from '../firebaseRequests/animals';
@@ -19,17 +20,18 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-
-        <Fish />
-        <Home />
-        <Inventory />
-        <Login />
-        <Navbar />
-        <New />
-        <Order />
-        <OrderSpa />
-        <Register />
-        <SingleOrder />
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Home}/>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
