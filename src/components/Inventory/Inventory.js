a511d7dff5f8cad0a735beff109d3b4a190cd4d1
Lola Simpson <lolalsimp@gmail.com>
2018-07-09 19:25:50 -0500

    getting fishes with axios

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,13 +1,35 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+
+import fishRequests from '../../firebaseRequests/fishes';
 
 import './Inventory.css';
 
 class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  }
+  componentDidMount () {
+    fishRequests
+      .getRequest()
+      .then((fishes) => {
+        this.setState({fishes: fishes});
+      })
+      .catch((err) => {
+        console.error('error with fish get request', err);
+      });
+  }
   render () {
+    const fishComponents = this.state.fishes.map((fish) => {
+      return (
+        <h2>{fish.name}</h2>
+      );
+    });
     return (
       <div className="Inventory">
         <h1>Inventory</h1>
+        <ul className="fishes">
+          {fishComponents}
+        </ul>
       </div>
     );
   }
