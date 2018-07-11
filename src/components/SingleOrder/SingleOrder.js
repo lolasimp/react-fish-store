745cf8eda7827f26238dc94232187eafe51b413b
Lola Simpson <lolalsimp@gmail.com>
2018-07-10 20:28:25 -0500

    deleting from firebase and dom

--- src/components/SingleOrder/SingleOrder.js
+++ src/components/SingleOrder/SingleOrder.js
@@ -2,12 +2,29 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 
 import './SingleOrder.css';
+import orderRequests from '../../firebaseRequests/orders';
 
 class SingleOrder extends React.Component {
+
+  deleteOrderClick = () => {
+    const firebaseId = this.props.match.params.id;
+    console.error('id', firebaseId);
+    orderRequests
+      .deleteRequest(firebaseId)
+      .then(() => {
+        this.props.history.push('/orders');
+      })
+      .catch((err) => {
+        console.error('error with delete', err);
+      });
+
+  };
+
   render () {
     return (
       <div className="SingleOrder">
         <h1>SingleOrder</h1>
+        <button className="btn btn-danger" onClick={this.deleteOrderClick}>Delete Order</button>
       </div>
     );
   }
